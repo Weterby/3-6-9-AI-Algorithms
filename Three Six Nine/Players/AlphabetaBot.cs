56a80a671d60c62ea7dfa52b0ebff8d2496cdc2f
Adam Schwenk <weterby2@gmail.com>
2021-11-28 19:21:08 +0100

    Change inheritance of EvaluateMove

--- Three Six Nine/Players/AlphabetaBot.cs
+++ Three Six Nine/Players/AlphabetaBot.cs
@@ -36,62 +36,7 @@ namespace Three_Six_Nine
             //Console.WriteLine("best: " + bestScore + ", index: " + move);
             return move;
         }
-        private int EvaluateMove(int[] gameState, int index)
-        {
-            int amount = 0;
-            int scoreV = 0;
-            int scoreH = 0;
-            int offset = 9;
-            int rowIndex = (index / offset) * offset;
-            int colIndex = index % offset;
-            for (int i = rowIndex; i < rowIndex + offset; i++)
-            {
-                if (gameState[i] == 1) amount++;
-            }
-
-            if (amount == 2) scoreH -= 5;
-            if (amount == 3) scoreH += 5;
-            if (amount == 5) scoreH -= 25;
-            if (amount == 6) scoreH += 25;
-            if (amount == 8) scoreH -= 125;
-            if (amount == 9) scoreH += 125;
-            amount = 0;
-
-            for (int i = colIndex; i < gameState.Length; i += 9)
-            {
-                if (gameState[i] == 1) amount++;
-            }
-
-            if (amount == 2) scoreV -= 5;
-            if (amount == 3) scoreV += 5;
-            if (amount == 5) scoreV -= 25;
-            if (amount == 6) scoreV += 25;
-            if (amount == 8) scoreV -= 125;
-            if (amount == 9) scoreV += 125;
-
-            int value = 0;
-            if (scoreH >= 0 && scoreV >= 0)
-            {
-                value = scoreH + scoreV;
-            }
-            else if (scoreH <= 0 && scoreV <= 0)
-            {
-                value = Math.Min(scoreH, scoreV);
-            }
-            else if (scoreH == -scoreV)
-            {
-                value = 0;
-            }
-            else if (scoreH == 0 && scoreV == 0)
-            {
-                value = 0;
-            }
-            else
-            {
-                value = scoreH + scoreV;
-            }
-            return value;
-        }
+       
         private int Alphabeta(int[] gameState, int depth, bool isMaximizing, int lastIndex, int alpha= -999, int beta = 999)
         {
             List<int> indexes = board.GetAllEmptyCellsIndexes(gameState);
