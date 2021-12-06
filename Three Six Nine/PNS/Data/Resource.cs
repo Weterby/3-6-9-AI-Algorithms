dbd1ec69366b21cc42476f199f1f6db375a8e6cf
Adam Schwenk <weterby2@gmail.com>
2021-12-06 20:15:37 +0100

    Add Resource.cs

---
+++ Three Six Nine/PNS/Data/Resource.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Three_Six_Nine.PNS.Data
+{
+    class Resource
+    {
+        private int maxAmount;
+        private int usedAmount;
+
+        public bool IsAvailable()
+        {
+            return usedAmount++ > maxAmount;
+        }
+
+        public void Reset()
+        {
+            usedAmount = 0;
+        }
+    }
+}
